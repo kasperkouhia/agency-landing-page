31cb4dde655c753543e0cce1ab6cb0b3c88d497b
Kasper Kouhia <60173278+kasperkouhia@users.noreply.github.com>
2025-07-18 01:02:40 +0300

    Change entry point to follow conventions

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/header";
+import About from "./components/about";
+import Testimonials from "./components/testimonials";
+import Gallery from "./components/gallery";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Testimonials />
+      <Gallery />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Header from "./components/header";
-import About from "./components/about";
-import Testimonials from "./components/testimonials";
-import Gallery from "./components/gallery";
-import Footer from "./components/footer";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Header />
-    <About />
-    <Testimonials />
-    <Gallery />
-    <Footer />
+    <App />
   </StrictMode>,
 );
